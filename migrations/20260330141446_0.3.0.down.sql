76e2915de33a1ea7a2169eede0a97c7fbcb195eb
OneProg <jade.weirdo@gmail.com>
2026-03-30 17:17:49 +0300

    Add revert migration to 0.3.0

---
+++ migrations/20260330141446_0.3.0.down.sql
@@ -0,0 +1,9 @@
+drop table submissions_subgroups_results;
+drop table submissions;
+drop table problems_subgroups;
+drop table problems;
+drop table users;
+
+drop type subgroup_type;
+drop type total_verdict;
+drop type subgroup_verdict;

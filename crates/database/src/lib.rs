b6778f8acdbd2567f15d452101b1626b650bb01f
OneProg <jade.weirdo@gmail.com>
2026-03-31 12:09:12 +0300

    Migrate back to sqlx methods

--- crates/database/src/lib.rs
+++ crates/database/src/lib.rs
@@ -10,10 +10,10 @@
 #![forbid(unsafe_code)]
 
 use models::{
-    problem_config::{DatabaseProblemConfig, Subgroup},
+    problem_config::DatabaseProblemConfig,
     verdicts::{SubgroupVerdict, TotalVerdict},
 };
-use sqlx::{PgPool, types::Json};
+use sqlx::PgPool;
 use tools::map::MapLogExt;
 
 /// Creates a user with login and password hash and returns it's id
@@ -24,11 +24,11 @@ pub async fn create_user(
     login: &str,
     password_hash: &str,
 ) -> Result<i64, TotalVerdict> {
-    let user_id = sqlx::query_scalar!(
+    let user_id = sqlx::query_scalar(
         r"insert into users (login, password_hash) values ($1, $2) returning id",
-        login,
-        password_hash
     )
+    .bind(login)
+    .bind(password_hash)
     .fetch_one(pool)
     .await
     .map_log(TotalVerdict::InvalidRequest)?;
@@ -43,9 +43,8 @@ pub async fn get_problem_config(
     pool: &PgPool,
     problem_id: i64,
 ) -> Result<DatabaseProblemConfig, TotalVerdict> {
-    let config = sqlx::query_as!(
-        DatabaseProblemConfig,
-        r#"select
+    let config = sqlx::query_as(
+        r"select
                 c.name,
                 c.time_limit_ms,
                 c.memory_limit_mb,
@@ -59,15 +58,15 @@ pub async fn get_problem_config(
                             'score', v.score,
                             'depends_on', v.depends_on
                         ) order by v.subgroup_index
-                    ), '[]'::json
-                ) as "subgroups!: Json<Vec<Subgroup>>"
+                    ), '[]'
+                ) as subgroups
             from problems c
             left join problems_subgroups v on v.problem_id = c.id
             where c.id = $1
             group by c.id, c.name, c.time_limit_ms, c.memory_limit_mb, c.checker_path, c.tests_path;
-        "#,
-        problem_id
+        ",
     )
+    .bind(problem_id)
     .fetch_one(pool)
     .await
     .map_log(TotalVerdict::InvalidRequest)?;
@@ -79,14 +78,13 @@ pub async fn get_problem_config(
 /// # Errors
 /// Returns an error if `problem_id` is invalid
 pub async fn insert_submission(pool: &PgPool, problem_id: i64) -> Result<i64, TotalVerdict> {
-    let submission_id = sqlx::query_scalar!(
+    let submission_id = sqlx::query_scalar(
         r"insert into submissions (problem_id, user_id, total_verdict, total_score) 
-          values ($1, $2, $3::total_verdict, $4) returning id",
-        problem_id,
-        None::<i64>,
-        TotalVerdict::Pending as _,
-        0
+          values ($1, $2, $3, $4) returning id",
     )
+    .bind(problem_id)
+    .bind(None::<i64>)
+    .bind(TotalVerdict::Pending)
     .fetch_one(pool)
     .await
     .map_log(TotalVerdict::InvalidRequest)?;
@@ -103,13 +101,13 @@ pub async fn update_total_testing_result(
     verdict: &TotalVerdict,
     score: i32,
 ) -> Result<(), TotalVerdict> {
-    sqlx::query!(
-        r"update submissions set total_verdict = $1::total_verdict, total_score = $2 
+    sqlx::query(
+        r"update submissions set total_verdict = $1, total_score = $2 
             where id = $3",
-        verdict as _,
-        score,
-        submission_id
     )
+    .bind(verdict)
+    .bind(score)
+    .bind(submission_id)
     .execute(pool)
     .await
     .map_log(TotalVerdict::InvalidRequest)?;
@@ -124,16 +122,16 @@ pub async fn insert_subgroup_testing_result(
     submission_id: i64,
     subgroup_id: i32,
 ) -> Result<(), TotalVerdict> {
-    sqlx::query!(
+    sqlx::query(
         r"insert into submissions_subgroups_results (subgroup_id, submission_id, subgroup_verdict, test, score, checker_msg)
             values ($1, $2, $3::subgroup_verdict, $4, $5, $6)",
-        subgroup_id,
-        submission_id,
-        SubgroupVerdict::Testing as _,
-        0,
-        0,
-        ""
     )
+        .bind(subgroup_id)
+        .bind(submission_id)
+        .bind(SubgroupVerdict::Testing)
+        .bind(0)
+        .bind(0)
+        .bind("")
         .execute(pool)
         .await
         .map_log(TotalVerdict::InvalidRequest)?;
@@ -152,16 +150,16 @@ pub async fn update_subgroup_testing_result(
     score: i32,
     checker_msg: String,
 ) -> Result<(), TotalVerdict> {
-    sqlx::query!(
+    sqlx::query(
         r"update submissions_subgroups_results set subgroup_verdict = $1::subgroup_verdict, test = $2, score = $3, checker_msg = $4 
             where submission_id = $5 and subgroup_id = $6",
-        verdict as _,
-        test,
-        score,
-        checker_msg,
-        submission_id,
-        subgroup_id
     )
+        .bind(verdict)
+        .bind(test)
+        .bind(score)
+        .bind(checker_msg)
+        .bind(submission_id)
+        .bind(subgroup_id)
         .execute(pool)
         .await
         .map_log(TotalVerdict::InvalidRequest)?;

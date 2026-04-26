3c9b910770b3f880391c1e88d3c593ab235d34c8
OneProg <jade.weirdo@gmail.com>
2026-04-26 14:44:10 +0300

    Fix migration

--- migrations/20260425183245_0.4.11-1.up.sql
+++ migrations/20260425183245_0.4.11-1.up.sql
@@ -5,6 +5,8 @@ create type admin_level_new as enum (
     'owner'
 );
 
+alter table users
+alter column admin_level drop default;
 update users set admin_level = 'not_admin';
 
 alter table users
@@ -14,3 +16,6 @@ using admin_level::text::admin_level_new;
 drop type admin_level;
 
 alter type admin_level_new rename to admin_level;
+
+alter table users
+alter column admin_level set default 'not_admin';

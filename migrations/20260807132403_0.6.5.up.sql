9e29af88fd4cc6b73fa684703f4fbdf716435132
OneProg <jade.weirdo@gmail.com>
2026-08-07 18:08:42 +0300

    Fix migration

--- migrations/20260807132403_0.6.5.up.sql
+++ migrations/20260807132403_0.6.5.up.sql
@@ -2,5 +2,5 @@ alter table contests add column hide_leaderboard boolean not null default 'false
 
 create table contests_co_authors (
     contest_id bigint references contests(id) on delete cascade,
-    user_id bigint references users(id) on delete cascade,
+    user_id bigint references users(id) on delete cascade
 );

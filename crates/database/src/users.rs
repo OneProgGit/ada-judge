3c4423fa4c6f4e4fcea585fa39b2e7fbf2f7dc04
Leonid Sergeev <leonid@Leonids-MacBook-Air.local>
2026-08-21 22:22:32 +0300

    Users database fixes

--- crates/database/src/users.rs
+++ crates/database/src/users.rs
@@ -6,13 +6,21 @@ use models::users::DatabaseUser;
 use sqlx::PgPool;
 
 pub async fn get_users(pool: &PgPool) -> Result<Vec<PrivateUserData>, AdaJudgeError> {
-    let users = sqlx::query_as::<_, DatabaseUser>("select * from users order by id")
-        .fetch_all(pool)
-        .await
-        .map_err(|_| AdaJudgeError::Internal)?
-        .iter()
-        .map(|x| x.clone().into())
-        .collect();
+    let users = sqlx::query_as!(
+        DatabaseUser,
+        r#"select id,
+            login,
+            password_hash,
+            admin_level as "admin_level: AdminLevel",
+            created_at
+            from users order by id"#,
+    )
+    .fetch_all(pool)
+    .await
+    .map_err(|_| AdaJudgeError::Internal)?
+    .iter()
+    .map(|x| x.clone().into())
+    .collect();
 
     Ok(users)
 }
@@ -22,11 +30,11 @@ pub async fn create_user(
     login: &str,
     password_hash: &str,
 ) -> Result<i64, AdaJudgeError> {
-    let user_id = sqlx::query_scalar(
+    let user_id = sqlx::query_scalar!(
         r#"insert into users (login, password_hash) values ($1, $2) returning id"#,
+        login,
+        password_hash
     )
-    .bind(login)
-    .bind(password_hash)
     .fetch_one(pool)
     .await
     .map_err(|e| {
@@ -43,8 +51,7 @@ pub async fn create_user(
 }
 
 pub async fn delete_user(pool: &PgPool, id: i64) -> Result<(), AdaJudgeError> {
-    sqlx::query(r#"delete from users where id = $1"#)
-        .bind(id)
+    sqlx::query!(r#"delete from users where id = $1"#, id)
         .execute(pool)
         .await
         .map_err(|_| AdaJudgeError::Internal)?;
@@ -57,31 +64,47 @@ pub async fn change_admin_level(
     id: i64,
     admin_level: &AdminLevel,
 ) -> Result<(), AdaJudgeError> {
-    sqlx::query(r#"update users set admin_level = $1 where id = $2"#)
-        .bind(admin_level)
-        .bind(id)
-        .execute(pool)
-        .await
-        .map_err(|_| AdaJudgeError::Internal)?;
+    sqlx::query!(
+        r#"update users set admin_level = $1 where id = $2"#,
+        admin_level.clone() as AdminLevel,
+        id
+    )
+    .execute(pool)
+    .await
+    .map_err(|_| AdaJudgeError::Internal)?;
 
     Ok(())
 }
 
 pub async fn get_user_by_login(pool: &PgPool, login: &str) -> Result<DatabaseUser, AdaJudgeError> {
-    sqlx::query_as(r#"select * from users where login = $1"#)
-        .bind(login)
-        .fetch_one(pool)
-        .await
-        .map_err(|e| match e {
-            sqlx::Error::RowNotFound => AdaJudgeError::NotFound,
-            _ => AdaJudgeError::Internal,
-        })
+    sqlx::query_as!(
+        DatabaseUser,
+        r#"select id,
+        login,
+        password_hash,
+        admin_level as "admin_level: AdminLevel",
+        created_at from users where login = $1"#,
+        login
+    )
+    .fetch_one(pool)
+    .await
+    .map_err(|e| match e {
+        sqlx::Error::RowNotFound => AdaJudgeError::NotFound,
+        _ => AdaJudgeError::Internal,
+    })
 }
 
 pub async fn get_user_by_id(pool: &PgPool, id: i64) -> Result<DatabaseUser, AdaJudgeError> {
-    sqlx::query_as(r#"select * from users where id = $1"#)
-        .bind(id)
-        .fetch_one(pool)
-        .await
-        .map_err(|_| AdaJudgeError::Internal)
+    sqlx::query_as!(
+        DatabaseUser,
+        r#"select id,
+        login,
+        password_hash,
+        admin_level as "admin_level: AdminLevel",
+        created_at from users where id = $1"#,
+        id
+    )
+    .fetch_one(pool)
+    .await
+    .map_err(|_| AdaJudgeError::Internal)
 }

8a0057d11afd49f291477facfdb16ae1968e017d
Leonid Sergeev <oneprog>
2026-08-29 19:27:43 +0300

    Fix

--- src/crypt.rs
+++ src/crypt.rs
@@ -1,15 +1,11 @@
 use aj_models::errors::AdaJudgeError;
-use argon2::{
-    Argon2, PasswordHash, PasswordHasher, PasswordVerifier,
-    password_hash::{SaltString, rand_core::OsRng},
-};
+use argon2::{Argon2, PasswordHash, PasswordHasher, PasswordVerifier};
 
 pub fn get_password_hash(password: &str) -> Result<String, AdaJudgeError> {
-    let salt = SaltString::generate(&mut OsRng);
     let argon2 = Argon2::default();
 
     let res = argon2
-        .hash_password(password.as_bytes(), &salt)
+        .hash_password(password.as_bytes())
         .map_err(|_| AdaJudgeError::Internal)?;
 
     Ok(res.to_string())

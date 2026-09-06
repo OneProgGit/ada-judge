3c5c21efeeb84e67a8163d5531e6ae6e37f1d29a
Leonid Sergeev <leonid@Leonids-MacBook-Air.local>
2026-09-06 21:19:01 +0300

    Fix

--- src/app_state.rs
+++ src/app_state.rs
@@ -6,10 +6,13 @@ use sqlx::PgPool;
 use std::sync::Arc;
 use tokio::sync::{Mutex, broadcast};
 
+type ContestsSubsType = DashMap<i64, broadcast::Sender<ContestEvent>>;
+type QuestionsSubsType = DashMap<(Option<i64>, i64), broadcast::Sender<ContestEvent>>;
+
 #[derive(Clone)]
 pub struct AppState {
     pub db: PgPool,
     pub apalis_backend: Arc<Mutex<RedisStorage<SubmissionTask>>>,
-    pub contests_subs: Arc<DashMap<i64, broadcast::Sender<ContestEvent>>>,
-    pub questions_subs: Arc<DashMap<(Option<i64>, i64), broadcast::Sender<ContestEvent>>>,
+    pub contests_subs: Arc<ContestsSubsType>,
+    pub questions_subs: Arc<QuestionsSubsType>,
 }

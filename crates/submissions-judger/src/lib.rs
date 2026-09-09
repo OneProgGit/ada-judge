6179326f13ee88014c38c02fd7555a9b490ed1cf
Leonid Sergeev <leonid@Leonids-MacBook-Air.local>
2026-09-09 19:57:48 +0300

    Fix back

--- crates/submissions-judger/src/lib.rs
+++ crates/submissions-judger/src/lib.rs
@@ -408,7 +408,6 @@ pub async fn test_submission(
                 .map_db(&pool, submission_id, Some((subgroup_index, test)))
                 .await
                 .map_err(|_| TestingVerdict::Fail)?;
-            return Err(TestingVerdict::Fail.into());
         }
 
         total_score += subgroup_result.score;
